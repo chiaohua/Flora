1aba175c99a43c91d76b1fd6a2af50a1e273b207
chiaohua <inin0112@gmail.com>
2025-07-08 16:50:35 +0800

    708

---
+++ js708/lott.js
@@ -0,0 +1,18 @@
+let sortBefore = document.getElementById("sortBefore");
+let sortAfter = document.getElementById("sortAfter");
+let sortNo = document.getElementById("sortNo");
+
+//威力彩
+function lott1() {
+    let lott = [];
+
+    //while
+
+    while (1) {
+        if (lott.length == 6) {//字串的長度
+            break;
+        }
+        let lottNo = Math.floor(Math.random() * 38) + 1;
+
+    }
+}
